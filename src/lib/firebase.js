5950f97fcafdc30964eafdccdde0bf9ad97ba3e4
alyshia bentley <alyshiabentley@gmail.com>
2022-04-01 22:03:08 -0700

    setup firebase and seed data

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// import the seed file
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyDIaE3eAGUlIqdM_7a8XdaNaCTWh7XrMms',
+  authDokmain: 'insta-clone-ea9b1.firebaseapp.com',
+  projectId: 'insta-clone-ea9b1',
+  storageBucet: 'insta-clone-ea9b1.appspot.com',
+  messagingSenderId: '233303138170',
+  appId: '1:233303138170:web:4d0f7672345509f8ef00c3'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// Onlu need to run this once!
+// seedDatabase(firebase);
+export { firebase, FieldValue };
