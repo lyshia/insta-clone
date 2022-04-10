e0d72b097c6e6eb071a4f99fa37d4ed27660547b
lyshia <alyshiabentley@gmail.com>
2022-04-09 21:51:54 -0700

    add tailwinds

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,25 @@
-const Login = (props) => <h1> Login page! </h1>;
+import { useNavigate } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react';
+import FirebaseContext from '../context/firebase';
+
+const Login = (props) => {
+  // useHistory is replaced by useNavigate();
+  const navigate = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - InstaClone';
+  }, []);
+
+  return <div className="text-3xl font-bold underline">Hello world!</div>;
+};
 
 export default Login;
