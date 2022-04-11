a6e0016b95daab313a1af2f0ff1ced510b7161cc
lyshia <alyshiabentley@gmail.com>
2022-04-10 21:48:28 -0700

    add form for login

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,7 +19,37 @@ const Login = (props) => {
     document.title = 'Login - InstaClone';
   }, []);
 
-  return <div className="text-3xl font-bold underline">Hello world!</div>;
-};
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary"> {error}</p>}
 
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="enter your email address"
+              type="text"
+              placeholder="email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="enter your password"
+              type="password"
+              placeholder="password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
