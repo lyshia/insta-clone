298d185e3109dba5480bc06c2058763f1a5b51f2
lyshia <alyshiabentley@gmail.com>
2022-04-19 22:35:20 -0700

    start user sidebar

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,5 +1,19 @@
-const User = () => {
-    return <p> I am a user</p>
-}
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+const User = ({ username, fullName }) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link to={`/p/${username}`} className="grid">
+      <p>{username}</p>
+    </Link>
+  );
+
+User.propTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string
+};
 
 export default User;
