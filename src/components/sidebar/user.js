5d1dd41d85aac7d90ce79a00e5afb6afc7142486
lyshia <alyshiabentley@gmail.com>
2022-04-20 21:13:21 -0700

    add user image to sidebar

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -6,8 +6,14 @@ const User = ({ username, fullName }) =>
   !username || !fullName ? (
     <Skeleton count={1} height={61} />
   ) : (
-    <Link to={`/p/${username}`} className="grid">
-      <p>{username}</p>
+    <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+        />
+      </div>
     </Link>
   );
 
