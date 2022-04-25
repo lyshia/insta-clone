05b3e30f7f6e56cfa627542a993e11a3780d91b1
lyshia <alyshiabentley@gmail.com>
2022-04-24 23:39:45 -0700

    get usernamer and fullname to show up on user

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,14 +1,14 @@
 import useUser from '../../hooks/use-user';
 import User from './user';
 import Suggestions from './suggestions';
-4
+
 const Sidebar = () => {
   const {
     user: { username, fullName, userId }
   } = useUser();
   return (
     <div className="p-4">
-      <User />
+      <User username={username} fullName={fullName} />
       <Suggestions />
     </div>
   );
